376a2e96c5da91b9e10eb85265ef5c9acf0ef897
Antoine276 <antoine.colombo276@gmail.com>
2022-11-20 17:06:19 +0100

    Evolution to allow start and end date of extra mortgage payment + Cleaning of debug prints

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -2,25 +2,24 @@
 #
 # Exercise 1.7
 
+# Parameters
 fPrincipal = 500000.0              # Principal to pay back
 fRate = 0.05                       # Borrowing annual rate
 fMensualRate = (fRate / 12.0)      # Borrowing rate mensualized
 fBaselinePayment = 2684.11         # Mensual baseline payment
 fExtraPayment = 1000.0             # Additionnal payment for the x first months
-iExtraPaymentDuration = 12         # Duration in months of the additionnal payment
+iExtraPaymentFirstMonth = 61       # First month of extra payment
+iExtraPaymentLastMonth = 108       # Last month of extra payment
 fTotalPaid = 0.0                   # Total currently paid
+
+# Declarations and Initialisation
 iMonth = 1                         # Current number of months
 fPayment = 0                       # Current payment
 
 
 while fPrincipal > 0.0:
-
-    # print('Month :', iMonth)
-    # print('Principal :', fPrincipal)
-    # print('MensualRate :', fMensualRate)
-    # print('Payment :', fPayment)
-
-    if iMonth <= iExtraPaymentDuration :
+    
+    if ((iMonth >= iExtraPaymentFirstMonth) and (iMonth <= iExtraPaymentLastMonth)) :
         fPayment = (fBaselinePayment + fExtraPayment)
     else :
         fPayment = fBaselinePayment
@@ -29,7 +28,5 @@ while fPrincipal > 0.0:
 
     fTotalPaid += (fPayment)
     iMonth += 1
-
-    # print('----------------------------------------')
 
 print('Total payed :', round(fTotalPaid, 2), 'in', iMonth, 'months !')
