4d8c801d6b6d7a624eef9858869f03dc2c36c0ea
Antoine276 <antoine.colombo276@gmail.com>
2022-11-20 16:12:13 +0100

    Implementation of bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,22 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# Parameters
+iHeight = 100                # Height from which to drop the ball
+iMaxBounce = 10              # Maximum number of bounces
+iBounceStrenght = (3/5)      # Intensity of bounce
+iDecimals = 4                # Decimal for rounding
+
+# Initialisations
+iBounce = 1
+
+
+while iBounce <= iMaxBounce :
+    iHeight = (iHeight * iBounceStrenght)
+    print('Bounce :', iBounce, 'Height :', round(iHeight, iDecimals))
+
+    iBounce += 1
+
+
+
