1f48a4f5e0f550008f3273b57dd71ae433d758a9
Antoine276 <antoine.colombo276@gmail.com>
2022-11-20 17:28:32 +0100

    Adding of mensual print about current mortgage state + Improvment of last month payment + Correction of comment typos

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,30 +3,43 @@
 # Exercise 1.7
 
 # Parameters
-fPrincipal = 500000.0              # Principal to pay back
+fInitialPrincipal = 500000.0       # Principal to pay back
 fRate = 0.05                       # Borrowing annual rate
 fMensualRate = (fRate / 12.0)      # Borrowing rate mensualized
 fBaselinePayment = 2684.11         # Mensual baseline payment
-fExtraPayment = 1000.0             # Additionnal payment for the x first months
+fExtraPayment = 1000.0             # Amount of extra payment
 iExtraPaymentFirstMonth = 61       # First month of extra payment
 iExtraPaymentLastMonth = 108       # Last month of extra payment
-fTotalPaid = 0.0                   # Total currently paid
 
 # Declarations and Initialisations
-iMonth = 1                         # Current number of months
+fTotalPaid = 0.0                   # Current total paid
+iMonth = 0                         # Current number of months
 fPayment = 0                       # Current payment
+fPrincipal = fInitialPrincipal     # Current principal remaining
 
 
 while fPrincipal > 0.0:
-    
-    if ((iMonth >= iExtraPaymentFirstMonth) and (iMonth <= iExtraPaymentLastMonth)) :
-        fPayment = (fBaselinePayment + fExtraPayment)
-    else :
-        fPayment = fBaselinePayment
 
-    fPrincipal = ((fPrincipal * (1 + fMensualRate)) - fPayment)
-
-    fTotalPaid += (fPayment)
+    # Incremenation of current month and principal
     iMonth += 1
+    fPrincipal *= (1.0 + fMensualRate)
+    
+    # Calculation of mensual payment
+    if not (fPayment > fPrincipal) :
+        if ((iMonth >= iExtraPaymentFirstMonth) and (iMonth <= iExtraPaymentLastMonth)) :
+            fPayment = (fBaselinePayment + fExtraPayment)
+        else :
+            fPayment = fBaselinePayment
+    else :
+        fPayment = fPrincipal
 
-print('Total payed :', round(fTotalPaid, 2), 'in', iMonth, 'months !')
+    # Calculation of remaining principal
+    fPrincipal = (fPrincipal - fPayment)
+
+    # Update of total currently paid
+    fTotalPaid += fPayment
+
+    print('Currently paid :', round(fTotalPaid, 2), 'in', iMonth, 'months.', 'Remaining :', round(fPrincipal, 2))
+
+
+print('Total paid :', round(fTotalPaid, 2), 'in', iMonth, 'months !')
